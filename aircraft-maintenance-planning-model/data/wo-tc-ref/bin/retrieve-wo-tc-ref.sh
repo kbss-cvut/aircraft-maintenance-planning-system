71fd9be2d5bd8e41942941176093084ae515eecb
Miroslav Blasko <blcham@gmail.com>
2021-08-30 20:11:00 +0200

    [New] wo-tc-ref refactored with stream resource

--- aircraft-maintenance-planning-model/data/wo-tc-ref/bin/retrieve-wo-tc-ref.sh
+++ aircraft-maintenance-planning-model/data/wo-tc-ref/bin/retrieve-wo-tc-ref.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-
 mkdir -p ./target
 
 SPIPES_SERVICE=http://localhost:8080/s-pipes
 
 WORKSPACE_DIR=/home/blcha/projects/20doprava-csat/git/aircraft-maintenance-planning-system/aircraft-maintenance-planning-model
 DATASET_TYPE=wo-tc-ref
-#DATASET_URL="https://docs.google.com/spreadsheets/d/1D7bI_0LsmgMvv-GFSZ2pQ_Pd0O2qegd-LWScR4gfGFw/export?format=csv&#gid=1994218813"
-DATASET_URL="https://docs.google.com/spreadsheets/d/1SPNR5T0drTc9Axnqzc193XjNPzyOSjnAx-pUGz1J0Nw/export?format=csv&#gid=977875978"
+INPUT_FILE_NAME=input.csv
+INPUT_FILE=./target/$INPUT_FILE_NAME
 OUTPUT_FILE=./target/output.jsonld
 FUNCTION_ID=transform-$DATASET_TYPE-data
-
 
 
 echo "==================================="
@@ -19,18 +17,20 @@ echo "INFO: s-pipes service url $SPIPES_SERVICE"
 echo "INFO: function id $FUNCTION_ID"
 echo "INFO: workspace dir $WORKSPACE_DIR"
 echo "INFO: dataset type $DATASET_TYPE"
-echo "INFO: dataset url $DATASET_URL"
+echo "INFO: input file $INPUT_FILE"
 echo "==================================="
 
 
+#DATASET_URL_ENCODED="`echo $DATASET_URL | urlencode.sh`"
 
-DATASET_URL_ENCODED="`echo $DATASET_URL | urlencode.sh`"
-
-URL="$SPIPES_SERVICE/service?_pId=$FUNCTION_ID&workspaceDir=$WORKSPACE_DIR&datasetType=$DATASET_TYPE&datasetUrl=$DATASET_URL_ENCODED"    
+URL="$SPIPES_SERVICE/service?_pId=$FUNCTION_ID&workspaceDir=$WORKSPACE_DIR&datasetType=$DATASET_TYPE&datasetResource=@$INPUT_FILE_NAME"    
 
 set -x
 
-#curl  -H 'Accept: text/json, text/plain, */*'  "$URL" > $OUTPUT_FILE
-curl  -H 'Accept: text/turtle'  "$URL" > $OUTPUT_FILE
+curl --location --request POST  \
+	--header 'Accept: text/turtle' \
+	--form 'files=@"/home/blcha/projects/20doprava-csat/git/aircraft-maintenance-planning-system/aircraft-maintenance-planning-model/data/wo-tc-ref/target/input.csv"' \
+	"$URL" > $OUTPUT_FILE
 
 set +x
+
