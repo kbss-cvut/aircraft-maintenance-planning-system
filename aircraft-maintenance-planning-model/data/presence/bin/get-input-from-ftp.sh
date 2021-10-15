bf0cfe82045408fb054e3668ae5bab4e68bb2194
Miroslav Blasko <blcham@gmail.com>
2021-10-16 00:38:28 +0200

    [New] Initial extraction of presence

--- aircraft-maintenance-planning-model/data/presence/bin/get-input-from-ftp.sh
+++ aircraft-maintenance-planning-model/data/presence/bin/get-input-from-ftp.sh
@@ -3,3 +3,6 @@
 DATASET_TYPE_DIR="$(dirname $(dirname $(realpath -s $0)))"
 
 $DATASET_TYPE_DIR/../bin/download-ftp-file.sh pritomnost.csv ./target/input.tsv
+
+#sed -i '1iEmployee No;status_1;status_2;date;time' ./target/input.tsv 
+sed -i '1s/^/Employee No;status_1;status_2;date;time\n/' ./target/input.tsv
