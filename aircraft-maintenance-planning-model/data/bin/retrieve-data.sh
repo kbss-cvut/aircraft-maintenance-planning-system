8dff6aca2f92172df8d801d52f1783ae5114b609
Miroslav Blasko <blcham@gmail.com>
2021-10-15 11:06:34 +0200

    [New] Help for retrieve data script

---
+++ aircraft-maintenance-planning-model/data/bin/retrieve-data.sh
@@ -0,0 +1,45 @@
+#!/bin/bash 
+
+INPUT_FILE=./target/input.tsv
+
+if [ ! "$#" -eq 1 ]; then
+        echo Retrieve DATASET_TYPE data based on existing tsv file located in file $INPUT_FILE wtihin DATASET_TYPE directory.
+        echo Usage :  $0 "DATASET_TYPE" 
+        echo Example:
+        echo "  $0 time-analysis"
+        exit
+fi
+
+
+DATASET_TYPE="$1"
+
+SPIPES_SERVICE=http://localhost:8080/s-pipes
+#SPIPES_SERVICE=https://kbss.felk.cvut.cz/s-pipes-csat
+
+DIR="$(dirname $(realpath -s $0))"
+
+OUTPUT_FILE=./target/output.ttl.txt
+FUNCTION_ID=transform-$DATASET_TYPE-data
+
+echo "==================================="
+echo "INFO: s-pipes service url $SPIPES_SERVICE"
+echo "INFO: function id $FUNCTION_ID"
+echo "INFO: dataset type $DATASET_TYPE"
+echo "INFO: input file $INPUT_FILE"
+echo "==================================="
+
+cd $DIR/../$DATASET_TYPE
+mkdir -p ./target
+
+URL="$SPIPES_SERVICE/service?_pId=$FUNCTION_ID&datasetResource=@$INPUT_FILE_NAME"    
+INPUT_FILE_ABSOLUTE="`realpath $INPUT_FILE`"
+
+set -x
+
+curl --location --request POST  \
+	--header 'Accept: text/turtle' \
+	--form 'files=@"'${INPUT_FILE_ABSOLUTE}'"' \
+	"$URL" > $OUTPUT_FILE
+
+set +x
+
