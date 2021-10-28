553223e512f30592df4aaf133910516e3f887262
Miroslav Blasko <blcham@gmail.com>
2021-10-28 22:34:19 +0200

    [Fix] Possibility to execute from any directory

--- aircraft-maintenance-planning-model/data/time-estimate/bin/get-input-from-ftp.sh
+++ aircraft-maintenance-planning-model/data/time-estimate/bin/get-input-from-ftp.sh
@@ -2,4 +2,6 @@
 
 DATASET_TYPE_DIR="$(dirname $(dirname $(realpath -s $0)))"
 
-$DATASET_TYPE_DIR/../bin/download-ftp-file.sh Dashboard2_time-estimate.csv ./target/input.tsv
+cd $DATASET_TYPE_DIR
+
+../bin/download-ftp-file.sh Dashboard2_time-estimate.csv ./target/input.tsv
