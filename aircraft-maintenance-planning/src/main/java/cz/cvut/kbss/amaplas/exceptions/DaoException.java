5a3d5bd26d5737704b84c8b73c53f097aa0710a0
Bogdan Kostov <kostobog@fel.cvut.cz>
2022-05-04 13:56:40 +0200

    [UPD] Update application exceptions

--- aircraft-maintenance-planning/src/main/java/cz/cvut/kbss/amaplas/exceptions/DaoException.java
+++
@@ -1,7 +0,0 @@
-package cz.cvut.kbss.amaplas.exceptions;
-
-public class DaoException extends ApplicationException {
-    public DaoException(Throwable cause) {
-        super(cause);
-    }
-}
--- aircraft-maintenance-planning/src/main/java/cz/cvut/kbss/amaplas/exceptions/InvalidParameterException.java
+++ aircraft-maintenance-planning/src/main/java/cz/cvut/kbss/amaplas/exceptions/InvalidParameterException.java
@@ -5,4 +5,12 @@ public class InvalidParameterException extends ApplicationException {
     public InvalidParameterException(String message) {
         super(message);
     }
+
+    public InvalidParameterException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidParameterException(Throwable cause) {
+        super(cause);
+    }
 }
