df0f2b2b7154d4de3bf10aa75d21a132e9b365a4
Bogdan Kostov <kostobog@fel.cvut.cz>
2022-05-04 15:00:29 +0200

    [UPD] Update application configuration: - fix configuration properties - Application.yml, ConfigProperties.java and Application.java - refactor percistance factory initialization - PersistenceConfig.java and MainPersistenceFactory.java

--- aircraft-maintenance-planning/src/main/java/cz/cvut/kbss/amaplas/Application.java
+++ aircraft-maintenance-planning/src/main/java/cz/cvut/kbss/amaplas/Application.java
@@ -1,11 +1,13 @@
 package cz.cvut.kbss.amaplas;
 
 
+import cz.cvut.kbss.amaplas.config.ConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ConfigProperties.class, ConfigProperties.Persistence.class, ConfigProperties.Repository.class})
 public class Application {
 
     public static void main(String[] args) {
