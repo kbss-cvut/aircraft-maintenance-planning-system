a38f955c2a85581b3b6b331da0037f37f0b96f4a
Matej Kulich <matej.kulich99@gmail.com>
2022-07-26 09:28:51 +0200

    [Feature #76] Remove manually added header

--- aircraft-maintenance-planning-model/data/presence/bin/get-input-from-ftp.sh
+++ aircraft-maintenance-planning-model/data/presence/bin/get-input-from-ftp.sh
@@ -5,6 +5,3 @@ DATASET_TYPE_DIR="$(dirname $(dirname $(realpath -s $0)))"
 cd $DATASET_TYPE_DIR
 
 ../bin/download-ftp-file.sh pritomnost.csv ./target/input.tsv
-
-#sed -i '1iEmployee No;status_1;status_2;date;time' ./target/input.tsv 
-#sed -i '1s/^/Employee No;status_1;status_2;date;time\n/' ./target/input.tsv
