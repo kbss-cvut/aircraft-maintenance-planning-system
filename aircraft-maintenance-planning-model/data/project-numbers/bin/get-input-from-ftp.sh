42a08be48ed3b79cae1faf21ea22aad3dc5698fa
Miroslav Blasko <blcham@gmail.com>
2022-10-27 23:14:37 +0200

    [Fix] Changing file extension to match shell scripts

--- aircraft-maintenance-planning-model/data/project-numbers/bin/get-input-from-ftp.sh
+++ aircraft-maintenance-planning-model/data/project-numbers/bin/get-input-from-ftp.sh
@@ -4,4 +4,4 @@ DATASET_TYPE_DIR="$(dirname $(dirname $(realpath -s $0)))"
 
 cd $DATASET_TYPE_DIR
 
-../bin/download-ftp-file.sh Dashboard2_project-numbers.csv ./target/input.tsv
+../bin/download-ftp-file.sh Dashboard2_project-numbers.csv ./target/input.csv
--- aircraft-maintenance-planning-model/data/wp-catalog/bin/get-input-from-ftp.sh
+++ aircraft-maintenance-planning-model/data/wp-catalog/bin/get-input-from-ftp.sh
@@ -4,4 +4,4 @@ DATASET_TYPE_DIR="$(dirname $(dirname $(realpath -s $0)))"
 
 cd $DATASET_TYPE_DIR
 
-../bin/download-ftp-file.sh Dashboard2_WP-catalog.csv ./target/input.tsv
+../bin/download-ftp-file.sh Dashboard2_WP-catalog.csv ./target/input.csv
