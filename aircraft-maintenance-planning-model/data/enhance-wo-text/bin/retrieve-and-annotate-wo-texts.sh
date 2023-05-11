11d31317c21ca622896e11e37f1bc48899508139
Matěj Kulich <67843655+Matthew-Kulich@users.noreply.github.com>
2023-05-11 16:49:13 +0200

    [Upd] Replace context by default in deploy script, remove unused script

--- aircraft-maintenance-planning-model/data/enhance-wo-text/bin/retrieve-and-annotate-wo-texts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DATASET_TYPE_DIR="$(dirname $(dirname $(realpath $0)))"
-DATASET_TYPE="$(basename "${DATASET_TYPE_DIR}")"
-
-# get input from rdf4j repository and save it to target/input.ttl
-"$DATASET_TYPE_DIR"/bin/get-input-from-rdf4j-repository.sh
