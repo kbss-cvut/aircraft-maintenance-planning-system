137b2208b6212f0ecfb3f7973cfc610da143c74f
Matěj Kulich <67843655+Matthew-Kulich@users.noreply.github.com>
2023-05-11 11:00:41 +0200

    [Upd] Add deploy-data script

--- aircraft-maintenance-planning-model/data/enhance-wo-text/bin/retrieve-and-annotate-wo-texts.sh
+++ aircraft-maintenance-planning-model/data/enhance-wo-text/bin/retrieve-and-annotate-wo-texts.sh
@@ -5,9 +5,3 @@ DATASET_TYPE="$(basename "${DATASET_TYPE_DIR}")"
 
 # get input from rdf4j repository and save it to target/input.ttl
 "$DATASET_TYPE_DIR"/bin/get-input-from-rdf4j-repository.sh
-
-# deploy slovnik.ttl and target/output.ttl.txt
-"$DATASET_TYPE_DIR"/../bin/deploy-rdf.sh slovnik.ttl
-"$DATASET_TYPE_DIR"/../bin/deploy-rdf.sh ./target/output.ttl.txt
-
-
