ea2e40493eddf8d27cddcacb96926c33e9d3120f
Matěj Kulich <67843655+Matthew-Kulich@users.noreply.github.com>
2023-05-15 10:01:33 +0200

    [Upd] Replace context by default, add parameter

--- aircraft-maintenance-planning-model/data/enhance-wo-text/bin/deploy-data.sh
+++ aircraft-maintenance-planning-model/data/enhance-wo-text/bin/deploy-data.sh
@@ -4,4 +4,4 @@ DATASET_TYPE_DIR="$(dirname $(dirname $(realpath -s $0)))"
 
 # deploy slovnik.ttl and target/output.ttl.txt
 "$DATASET_TYPE_DIR"/../bin/deploy-rdf.sh slovnik.ttl
-"$DATASET_TYPE_DIR"/../bin/deploy-rdf.sh ./target/output.ttl.txt
+"$DATASET_TYPE_DIR"/../bin/deploy-rdf.sh ./target/output.ttl.txt --append-context
