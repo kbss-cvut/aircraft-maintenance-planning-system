05306ec3859d1932800bcc22014b241448889ca7
Miroslav Blasko <blcham@gmail.com>
2023-05-26 19:02:34 +0200

    [Fix] Fix append-only deployment

--- aircraft-maintenance-planning-model/data/bin/deploy-rdf.sh
+++ aircraft-maintenance-planning-model/data/bin/deploy-rdf.sh
@@ -15,5 +15,5 @@ FILE="$1"
 ls "$FILE" | while read -r ONTOLOGY_FILE; do
   ONTOLOGY_IRI=$(awk '/(rdf:type|a).*owl:Ontology/{print prev1; print $0; getline; print; exit} {prev1 = $0}' "$ONTOLOGY_FILE" | awk -F'[<>]' '/^<http:/{print $2; exit}')
   echo INFO: Deploying "$ONTOLOGY_FILE" into context "$ONTOLOGY_IRI" ...
-	 ../../bin/rdf4j-deploy-context.sh "$REPLACE_PARAMETER" -C 'text/turtle' -s "$SERVER" -r "$REPOSITORY_NAME" -c "$ONTOLOGY_IRI" "$ONTOLOGY_FILE"
+	 ../../bin/rdf4j-deploy-context.sh $REPLACE_PARAMETER -C 'text/turtle' -s "$SERVER" -r "$REPOSITORY_NAME" -c "$ONTOLOGY_IRI" "$ONTOLOGY_FILE"
 done
