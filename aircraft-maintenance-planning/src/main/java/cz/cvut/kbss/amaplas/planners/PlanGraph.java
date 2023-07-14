92312f8e73d45cac619c9b2ec4799d241bd70d23
Bogdan Kostov <kostobog@fel.cvut.cz>
2023-07-14 09:25:56 +0200

    [UPD] Add serialization to gml format for PlanGraph - generated graphml format not compatible with yEd. Add method in GraphmlUtils to serialize to gml. - Create a gml exporter in PlanGraph

--- aircraft-maintenance-planning/src/main/java/cz/cvut/kbss/amaplas/planners/PlanGraph.java
+++ aircraft-maintenance-planning/src/main/java/cz/cvut/kbss/amaplas/planners/PlanGraph.java
@@ -2,9 +2,16 @@ package cz.cvut.kbss.amaplas.planners;
 
 import cz.cvut.kbss.amaplas.model.TaskExecution;
 import cz.cvut.kbss.amaplas.model.TaskType;
+import cz.cvut.kbss.amaplas.utils.GraphmlUtils;
 import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.nio.Attribute;
+import org.jgrapht.nio.GraphExporter;
+import org.jgrapht.nio.gml.GmlExporter;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class PlanGraph extends DefaultDirectedGraph<TaskPattern, SequencePattern> {
@@ -27,5 +34,26 @@ public class PlanGraph extends DefaultDirectedGraph<TaskPattern, SequencePattern
         return taskPattern;
     }
 
+    public static Function<TaskPattern, String> vertexIdProvider = tp -> tp.getTaskType().getCode();
+    public static Function<TaskPattern, Map<String, Attribute>> vertexAttributeProvider =
+            GraphmlUtils.createDefaultAttributeProvider(
+                    TaskPattern.class,
+                    (TaskPattern tp) -> Optional.ofNullable(tp.getTaskType()).map(tt ->
+                            tt.getCode() + " -- " +
+                            (tt.getDefinition() != null ? tt.getDefinition() : tt).getArea() + " -- " +
+                            escape(tt.getTitle())
+                    ).orElse(""));
 
+    public static String escape(String str){
+        return Optional.ofNullable(str).map(s -> s.replaceAll("(&)", "_")).orElse("");
+    }
+
+    public static GraphExporter<TaskPattern, SequencePattern> exporter;
+    {
+        GmlExporter<TaskPattern, SequencePattern> e = new GmlExporter<>();
+//        e.setVertexIdProvider(vertexIdProvider);
+        e.setVertexAttributeProvider(vertexAttributeProvider);
+        e.setParameter(GmlExporter.Parameter.EXPORT_VERTEX_LABELS,true);
+        exporter = e;
+    }
 }
