2db8a80bb620ff103e0206b1fb46eae1118f4965
Miroslav Blasko <blcham@gmail.com>
2023-10-10 20:22:06 +0200

    [Upd] Improve documentation

--- aircraft-maintenance-planning-model/data/bin/1-eml-filter-all.sh
+++ aircraft-maintenance-planning-model/data/bin/1-eml-filter-all.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+
+# prepare eml tabular data for next processsing 
+# 	copy "eml tabular data" --> "input tabular data" + mark "eml tabular data"
+
 SCRIPT_DIR=$(dirname $(readlink -m $0))
 
 INPUT_EML_TABULAR_DATA=./private/1-eml-tabular-data
 OUTPUT_TABULAR_DIR=./private/2-input-tabular-data
 
+echo 'INFO: Setting eml tabular data as input data for next processing'
 cp -v -r $INPUT_EML_TABULAR_DATA/20* $OUTPUT_TABULAR_DIR/
+
+echo 'INFO: Marking eml tabular data as processed (DONE)'
+mkdir -p $INPUT_EML_TABULAR_DATA/done/
 mv -v  $INPUT_EML_TABULAR_DATA/20* $INPUT_EML_TABULAR_DATA/done/
